e670277f0c7e5b8c7316735d799f3e808316b83d
Juwon <hardejuwon@gmail.com>
2023-07-13 10:23:35 +0100

    api backend set up

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,6 @@
+class Api:: MessagesController < ApplicationController
+  def random_greeting
+    @greeting = Message.all.sample
+    render json: @greeting
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    resources :messages, only: [] do
+      collection do
+        get 'random_greeting'
+      end
+    end
+  end
 end
